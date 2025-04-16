464304a3b62b7a54f1db8adb26444a656c3e6a4c
Agatabrete <144770128+Agatabrete@users.noreply.github.com>
2025-04-16 14:00:23 +0300

    juhei

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    $db_server = 'localhost';
+    $db_andmebaas = 'sport';
+    $db_kasutaja = 'agata';
+    $db_salasona = 'Passw0rd';
+
+    $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+    if (!$yhendus) {
+        die ("Probleem andmebaasiga");
+    }
+?>
