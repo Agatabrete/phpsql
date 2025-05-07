4899713fea5c476b73052f1a6cbc4983dcdd12df
Agatabrete <144770128+Agatabrete@users.noreply.github.com>
2025-05-07 15:12:25 +0300

    jes

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<?php include("config.php"); ?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin area</title>
+</head>
+<body>
+    
+</body>
+</html>
